e3158f1ded0db24915759250ed2b0f3002de2b47
Cristiane Mayara <cris.mayara@gmail.com>
2018-02-05 16:01:12 -0300

    Adding tabs

--- app.js
+++ app.js
@@ -5,6 +5,18 @@
     this.products = gems;
   });
 
+  app.controller("PanelController", function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(chackTab) {
+      return this.tab === chackTab;
+    };
+  });
+
   var gems = [
     {
       name: "Dodecahedron",
